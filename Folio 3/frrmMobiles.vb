af768cabb5dd37bbc273020b5fed5826bf585397
Justin <justintan0035@gmail.com>
2020-03-19 20:56:22 +1100

    Added internal documentation

--- Folio 3/frrmMobiles.vb
+++ Folio 3/frrmMobiles.vb
@@ -8,23 +8,23 @@ Public Class frrmMobiles
         dlgBrowseFiles.InitialDirectory = FileSystem.CurDir
         dlgBrowseFiles.FileName = "mobiles.txt"
 
-        Dim dlgResult = dlgBrowseFiles.ShowDialog()
+        Dim dlgResult = dlgBrowseFiles.ShowDialog() 'Open file browser to select file
         If dlgResult = DialogResult.OK Then
             Dim fileReader As System.IO.StreamReader
-            lstNumbers.Items.Clear()
-            fileReader = My.Computer.FileSystem.OpenTextFileReader(dlgBrowseFiles.FileName)
+            lstNumbers.Items.Clear() 'Clears all items so that previous items are removed
+            fileReader = My.Computer.FileSystem.OpenTextFileReader(dlgBrowseFiles.FileName) 'This creates a text reader which allows the program to read the textfile
             Dim line As String
             Dim lineNumber As Int32 = 0
-            While Not fileReader.EndOfStream
+            While Not fileReader.EndOfStream 'While not at the end of the file, the file reader will conitnue to read the names and mobile numbers
                 line = fileReader.ReadLine()
                 Dim row = line.Split(",")
                 Dim person As Person = New Person
                 person.Name = row(0) + " " + row(1)
                 Dim mobileNumber As String = row(2)
-                person.Mobile = mobileNumber.Substring(0, 4) + " " + mobileNumber.Substring(4, 3) + " " + mobileNumber.Substring(7, 3)
+                person.Mobile = mobileNumber.Substring(0, 4) + " " + mobileNumber.Substring(4, 3) + " " + mobileNumber.Substring(7, 3) 'Formatting for the phone number
                 lstNumbers.Items.Add(person.Name)
                 lstNumbers.Items(lineNumber).SubItems.Add(person.Mobile)
-                lineNumber = lineNumber + 1
+                lineNumber = lineNumber + 1 'Add the next person and their mobile number to the next line
             End While
             frrmMobiles.ActiveForm.Text = "Friends (file:" + dlgBrowseFiles.FileName + ")"
             fileReader.Close()
